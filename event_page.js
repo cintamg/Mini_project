ce455f5c8e56af6ae2136867bb7d4942e568947c
cintamg <cintaelamthottam@gmail.com>
2022-07-22 23:48:48 +0530

    final changes

--- event_page.js
+++ event_page.js
@@ -22,10 +22,30 @@ window.addEventListener('load', () => {
 		task_input_el.value = task;
 		task_input_el.setAttribute('readonly', 'readonly');
 
-		task_content_el.appendChild(task_input_el);
+		//task_content_el.appendChild(task_input_el);
 
 		const task_actions_el = document.createElement('div');
 		task_actions_el.classList.add('actions');
+
+        const task_check_el = document.createElement('input')
+        task_check_el.classList.add('check');
+        task_check_el.type = 'checkbox';
+		task_check_el.value = "value";
+		task_check_el.name = "name";
+		task_check_el.id = "id";
+
+		const task_budget_el = document.createElement('input')
+        task_budget_el.classList.add('budget');
+        task_budget_el.type = 'number';
+		task_budget_el.placeholder = "Budget";
+
+		const task_sub_el = document.createElement('textarea')
+        task_sub_el.classList.add('sub');
+        // task_sub_el.type = 'textarea';
+		task_sub_el.maxLength = "1000";
+        task_sub_el.rows = "3";
+		task_sub_el.cols = "40";
+		task_sub_el.placeholder = "Subcoordinator";
 		
 		const task_edit_el = document.createElement('button');
 		task_edit_el.classList.add('edit');
@@ -35,7 +55,11 @@ window.addEventListener('load', () => {
 		task_delete_el.classList.add('delete');
 		task_delete_el.innerText = 'Delete';
 
-
+		task_actions_el.appendChild(task_check_el);
+		task_content_el.appendChild(task_input_el);
+        //task_actions_el.appendChild(task_check_el);
+		task_actions_el.appendChild(task_budget_el);
+		task_actions_el.appendChild(task_sub_el);
 		task_actions_el.appendChild(task_edit_el);
 		task_actions_el.appendChild(task_delete_el);
 
@@ -59,5 +83,13 @@ window.addEventListener('load', () => {
 		task_delete_el.addEventListener('click', (e) => {
 			list_el.removeChild(task_el);
 		});
+
+		// task_check_el.addEventListener('click', (e) => {
+		// 	console.log("Checked");
+		// })
+
+		// document.getElementById("myButton").onclick = function () {
+		// 	location.href = "www.yoursite.com";
+		// };
 	});
 });
